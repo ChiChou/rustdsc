e928bb9945e3616502601789a80352b315e33464
cc <codecolorist@gmail.com>
2026-02-06 20:24:31 +0100

    change directory structure

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 for dir in corpus/* ; do
-    dsc=$(ls $dir/*/dyld_shared_cache_* | head -n 1)
+    dsc=$(ls $dir/dyld_shared_cache_* | head -n 1)
     cargo run -- sections $dsc
 done
